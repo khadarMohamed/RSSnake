cea54cec2332fc0be1919d49d198224fb129496e
Cuyler Eastman <60369884+CuylerE@users.noreply.github.com>
2020-04-08 16:44:32 -0600

    Add files via upload

--- View/RSSFeed.py
+++ View/RSSFeed.py
@@ -1,41 +1,40 @@
-import feedparser
+from bs4 import BeautifulSoup
 from tkinter import *
+from urllib.request import urlopen
 import webbrowser
+import lxml
 
-
-
-feed = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml?edition=us")
-
-feed_title = feed['feed']['title']
-feed_entries = feed.entries
-
-
+def cyclelink(x):
+    entry = news_list[x]
+    link = Label(root, text=entry.link.title)
+    link.pack()
+    link.bind("<Button-1>" ,lambda event, link=entry.link.text: callback(event, link))
+    x = x + 1
+def quit():
+    root.destroy()
 def callback(event, article_link):
     webbrowser.open_new(article_link)
+    
+parse_xml_url = urlopen("http://feeds.bbci.co.uk/news/rss.xml?edition=us")
+xml_page = parse_xml_url.read()
+parse_xml_url.close()
+feed = BeautifulSoup(xml_page, "xml")
+news_list = feed.findAll("item")
 
 
 root = Tk()
 text = Text(root)
-for entry in feed.entries:
-
-    article_title = entry.title
-    article_link = entry.link
-    article_published_at = entry.published # Unicode string
-    article_published_at_parsed = entry.published_parsed # Time object
-    #article_author = entry.author
-    content = entry.summary
-    #article_tags = entry.tags
-
-    print ("{}[{}]".format(article_title, article_link))
-    print ("Published at {}".format(article_published_at))
-    #print ("Published by {}".format(article_author))
-    print("Content {}".format(content))
-    #print("catagory{}".format(article_tags))
-
-    link = Label(root, text="{}\n".format(article_title), fg="blue", cursor="hand2")
-    link.pack()
-
-    link.bind("<Button-1>" ,lambda event, link=article_link: callback(event, link))
-
-text.tag_config("here")
+x = 0
+root.overrideredirect(True)
+entry = news_list[x]
+link = Label(root, text=entry.link.title)
+link.pack(side=LEFT)
+link.bind("<Button-1>" ,lambda event, link=entry.link.text: callback(event, link))
+button = Button(root, text = 'Click and Quit', command=quit)
+button.pack()
+root.after(1000, cyclelink(x))    
 root.mainloop()
+
+
+
+    
