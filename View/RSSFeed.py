a9c26dfbdfcbbed8661e3789c49cf1e101f64f09
Cuyler Eastman <60369884+CuylerE@users.noreply.github.com>
2020-04-12 14:41:38 -0600

    Added the beginnings for formatting the label and a proper loop for the feed.

--- View/RSSFeed.py
+++ View/RSSFeed.py
@@ -17,7 +17,7 @@ class Feed:
         self.entrynum = 0
         entry = self.news_list[self.entrynum]
        
-        self.label = Label(parent, text=entry.title.contents)
+        self.label = Label(parent, text=str(entry.title.contents)[2:-2], font = "Arial 20")
         self.label.bind("<Button-1>" ,lambda event, link=entry.link.text: self.callback(event, link))
         self.label.pack(side=LEFT)
         self.button = Button(root, text = 'Click and Quit', command=self.quit)
@@ -27,11 +27,16 @@ class Feed:
 
     def refresh_label(self):
         """ refresh the content of the label every second """
-        # increment the entry
-        self.entrynum += 1
+        #if we've reached the end of our list, cycle back to the start
+        if(self.entrynum < len(self.news_list)):
+            # increment the entry
+            self.entrynum += 1
+        else:
+            self.entrynum = 0
+            
         # display the new entry
         entry = self.news_list[self.entrynum]
-        self.label.config(text="%s" % entry.title.contents)
+        self.label.config(text="%s" % str(entry.title.contents)[2:-2])
         # request tkinter to call self.refresh after 1s (the delay is given in ms)
         self.label.after(3000, self.refresh_label)
     def quit(self):
