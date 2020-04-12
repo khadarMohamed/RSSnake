d903ed229b88bf824765b6ae7aa5c69b35d9f788
Cuyler Eastman <60369884+CuylerE@users.noreply.github.com>
2020-04-12 14:26:16 -0600

    RSS Feed updates each link
    Now only requires formatting and Font changes.

--- View/RSSFeed.py
+++ View/RSSFeed.py
@@ -3,38 +3,44 @@ from tkinter import *
 from urllib.request import urlopen
 import webbrowser
 import lxml
+from pip._internal import self_outdated_check
 
-def cyclelink(x):
-    entry = news_list[x]
-    link = Label(root, text=entry.link.title)
-    link.pack()
-    link.bind("<Button-1>" ,lambda event, link=entry.link.text: callback(event, link))
-    x = x + 1
-def quit():
-    root.destroy()
-def callback(event, article_link):
-    webbrowser.open_new(article_link)
-    
-parse_xml_url = urlopen("http://feeds.bbci.co.uk/news/rss.xml?edition=us")
-xml_page = parse_xml_url.read()
-parse_xml_url.close()
-feed = BeautifulSoup(xml_page, "xml")
-news_list = feed.findAll("item")
+class Feed:
+    def __init__(self, parent):
+        
+        parse_xml_url = urlopen("http://rss.cnn.com/rss/cnn_topstories.rss")
+        xml_page = parse_xml_url.read()
+        parse_xml_url.close()
+        
+        self.source = BeautifulSoup(xml_page, "xml")
+        self.news_list = self.source.findAll("item")
+        self.entrynum = 0
+        entry = self.news_list[self.entrynum]
+       
+        self.label = Label(parent, text=entry.title.contents)
+        self.label.bind("<Button-1>" ,lambda event, link=entry.link.text: self.callback(event, link))
+        self.label.pack(side=LEFT)
+        self.button = Button(root, text = 'Click and Quit', command=self.quit)
+        self.button.pack()
+        # start the timer
+        self.label.after(3000, self.refresh_label)
 
+    def refresh_label(self):
+        """ refresh the content of the label every second """
+        # increment the entry
+        self.entrynum += 1
+        # display the new entry
+        entry = self.news_list[self.entrynum]
+        self.label.config(text="%s" % entry.title.contents)
+        # request tkinter to call self.refresh after 1s (the delay is given in ms)
+        self.label.after(3000, self.refresh_label)
+    def quit(self):
+        root.destroy()
+    def callback(self, event, article_link):
+        webbrowser.open_new(article_link)
 
-root = Tk()
-text = Text(root)
-x = 0
-root.overrideredirect(True)
-entry = news_list[x]
-link = Label(root, text=entry.link.title)
-link.pack(side=LEFT)
-link.bind("<Button-1>" ,lambda event, link=entry.link.text: callback(event, link))
-button = Button(root, text = 'Click and Quit', command=quit)
-button.pack()
-root.after(1000, cyclelink(x))    
-root.mainloop()
-
-
-
-    
+if __name__ == "__main__":
+    root = Tk()
+    timer = Feed(root)
+    root.overrideredirect(True)
+    root.mainloop()
